85fb6b6ac943d4b4fa7d0801cd6d8f893762d84f
Polina <deadvinny1@gmail.com>
2023-07-11 20:00:28 +0300

    Добавлены файлы семинара 2

---
+++ task14/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая принимает на вход число и проверяет, кратно ли оно одновременно
+// 7 и 23.
+
+// 14 -> нет
+// 46 -> нет
+// 161 -> да
+
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+int num1 = 7;
+int num2 = 23;
+int div1 = number % num1;
+int div2 = number % num2;
+
+if (div1 == 0 && div2 == 0) 
+    {
+        Console.WriteLine($"Число {number} кратно 7 и 23.");
+    }
+else 
+    {
+        Console.WriteLine($"не кратно");
+    }
---
+++ task16/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая принимает на вход два числа и проверяет, является ли одно число квадратом другого.
+
+// 5, 25 -> да
+// -4, 16 -> да
+// 25, 5 -> да
+// 8,9 -> нет
+
+Console.Write("Введите число a: ");
+int a = int.Parse(Console.ReadLine());
+Console.Write("Введите число b: ");
+int b = int.Parse(Console.ReadLine());
+
+if (a * a == b || b * b == a) 
+    {
+        Console.WriteLine($"{a}, {b} -> да");
+    }
+else 
+    {
+        Console.WriteLine($"{a}, {b} -> нет");
+    }
